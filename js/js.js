260ef9713b9a0c77e16a1dbd5bb6fa231c9111b4
Sashaq15 <Sashaq15@tut.by>
2023-07-01 22:53:45 +0300

    first commit

---
+++ js/js.js
@@ -0,0 +1,38 @@
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130,
+    Artem: "dsf"
+  }
+
+
+
+function isEmpty(obj) {
+    for (let key in obj) {
+      // если тело цикла начнет выполняться - значит в объекте есть свойства
+      return false;
+    }
+    return true;
+  }
+
+
+function multiplyObjects(obj) {
+    for(let key in obj) {
+        if (typeof(obj[key]) == 'number') {
+            obj[key] = obj[key]*2;
+        }
+    }
+}
+
+for(let key in salaries) {
+    console.log(salaries[key])
+}
+
+multiplyObjects(salaries);
+
+for(let key in salaries) {
+    console.log(salaries[key])
+}
+
+
+console.log(typeof(2));
